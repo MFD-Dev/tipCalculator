2b1d0a5b26fa6a338ee807c65d8653f2e8e7b13e
Michael <Michaelflowersdev@gmail.com>
2021-07-14 22:58:18 -0500

    initial setup.  Need to tweak calculations

--- app.js
+++ app.js
@@ -6,3 +6,34 @@
 // -The form should also let the user choose the percentage tip they want to give.
 // -When they submit the form, show them the total with tip added.
 // -Display the total tip amount as well so they know how much they're tipping.
+
+// get access to 3 input boxes
+
+const amount = document.getElementById('amount');
+const guest = document.getElementById('guest');
+const quality = document.getElementById('quality');
+const tipAmount = document.getElementById('tip-amount');
+
+calculate = () => {
+    const tip =  ((amount.value * quality.value) /(guest.value)).toFixed(2); //return 2 dec
+    //clear the fields
+    amount.value = '';
+    guest.value = '';
+    quality.value = '';
+    // checks for NaN
+    if(tip === 'NaN') {
+        tipAmount.innerHTML = 'Tip $0 each';
+        showTipAmount();
+    }
+    else {
+        tipAmount.innerHTML = 'Tip $' + tip + ' each';
+        showTipAmount();
+    }
+}
+
+showTipAmount = () => {
+    var x = tipAmount;
+    x.className = 'show'; // this will show initiate the hidden feature and display animation
+    //setTimeout to change className from show, back to what it was
+    setTimeout(function(){x.className = x.className.replace('show', '');}, 3000);
+}
